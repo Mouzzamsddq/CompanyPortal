14aeeb64a9deb757b3b72d7f922c8fa2641a6ec6
Mouzzam Siddiqui <mouzzam_siddiqui.scsebtech@galgotiasuniversity.edu.in>
2021-10-18 09:51:45 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/companyportal/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.companyportal;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //code to remove the action bar and status bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        //open main activity after 3 sec
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //open main activity
+                startActivity(new Intent(SplashScreen.this , MainActivity.class));
+                finish();
+            }
+        },2500);
+
+
+    }
+}
